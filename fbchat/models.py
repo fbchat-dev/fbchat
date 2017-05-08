94682dbe1abfe5be26cb81982b78c08099732edb
Dainius <dainius.dauksevicius@gmail.com>
2017-05-08 22:34:15 +0300

    Revert "add event hooks, change method names"

--- fbchat/models.py
+++ fbchat/models.py
@@ -1,7 +1,5 @@
 from __future__ import unicode_literals
-from enum import Enum
 import sys
-
 
 class Base():
     def __repr__(self):
@@ -10,7 +8,6 @@ class Base():
 
     def __unicode__(self):
         return u'<%s %s (%s)>' % (self.type.upper(), self.name, self.url)
-
 
 class User(Base):
     def __init__(self, data):
@@ -25,62 +22,10 @@ class User(Base):
 
         self.data = data
 
-    @staticmethod
-    def adaptFromChat(user_in_chat):
-        """ Adapts user info from chat to User model acceptable initial dict
-
-        :param user_in_chat: user info from chat
-
-        'dir': None,
-        'mThumbSrcSmall': None,
-        'is_friend': False,
-        'is_nonfriend_messenger_contact': True,
-        'alternateName': '',
-        'i18nGender': 16777216,
-        'vanity': '',
-        'type': 'friend',
-        'searchTokens': ['Voznesenskij', 'Sergej'],
-        'thumbSrc': 'https://fb-s-b-a.akamaihd.net/h-ak-xfa1/v/t1.0-1/c9.0.32.32/p32x32/10354686_10150004552801856_220367501106153455_n.jpg?oh=71a87d76d4e4d17615a20c43fb8dbb47&oe=59118CE4&__gda__=1493753268_ae75cef40e9785398e744259ccffd7ff',
-        'mThumbSrcLarge': None,
-        'firstName': 'Sergej',
-        'name': 'Sergej Voznesenskij',
-        'uri': 'https://www.facebook.com/profile.php?id=100014812758264',
-        'id': '100014812758264',
-        'gender': 2
-        """
-
-        return {
-            'type': 'user',
-            'uid': user_in_chat['id'],
-            'photo': user_in_chat['thumbSrc'],
-            'path': user_in_chat['uri'],
-            'text': user_in_chat['name'],
-            'score': '',
-            'data': user_in_chat,
-        }
-
-
 class Thread():
     def __init__(self, **entries): 
         self.__dict__.update(entries)
 
-
 class Message():
     def __init__(self, **entries):
         self.__dict__.update(entries)
-
-
-class ThreadType(Enum):
-    USER = 1
-    GROUP = 2
-
-
-class TypingStatus(Enum):
-    Deleted = 0
-    Typing = 1
-
-
-class Sticker(Enum):
-    LARGE = '369239383222810'
-    MEDIUM = '369239343222814'
-    SMALL = '369239263222822'
