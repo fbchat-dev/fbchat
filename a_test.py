cc29b7032c6d2f2b6f81a05cb880fce39b3d2f59
Mads Marquart <madsmtm@gmail.com>
2017-12-27 21:01:34 +0100

    Fixed another small mistake

--- a_test.py
+++ a_test.py
@@ -11,6 +11,6 @@ logging.basicConfig(level=logging.DEBUG)
 print('Attempting a login {}, password: {} (Will be changed!)...'.format(environ['FBCHAT_EMAIL'], environ['FBCHAT_PASSWORD']))
 client = Client(environ['FBCHAT_EMAIL'], environ['FBCHAT_PASSWORD'], logging_level=logging.DEBUG)
 
-client.send(Message(text='Test'), thread_id=self.uid, thread_type=ThreadType.USER)
+client.send(Message(text='Test'), thread_id=client.uid, thread_type=ThreadType.USER)
 
 client.logout()
