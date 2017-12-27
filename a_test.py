5116a0cffdfa3dd05558f153654160fe991b1446
Mads Marquart <madsmtm@gmail.com>
2017-12-27 20:51:36 +0100

    Made an attempt to login to facebook with Travis

---
+++ a_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import logging
+from os import environ
+from fbchat import Client
+from fbchat.models import *
+
+logging.basicConfig(logging_level=logging.DEBUG)
+
+print('Attempting a login {}, password: {} (Will be changed!)...'.format(environ['FBCHAT_EMAIL'], environ['FBCHAT_PASSWORD']))
+client = Client(environ['FBCHAT_EMAIL'], environ['FBCHAT_PASSWORD'], logging_level=logging.DEBUG)
+
+client.send(Message(text='Test'), thread_id=self.uid, thread_type=ThreadType.USER)
+
+client.logout()
