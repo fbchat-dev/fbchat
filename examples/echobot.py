95f018fad3b5a9fa2312f1eaecc9a16cb70e78a2
Mads Marquart <madsmtm@gmail.com>
2018-03-19 21:40:51 +0100

    Fixed example `Echobot`

--- examples/echobot.py
+++ examples/echobot.py
@@ -5,8 +5,8 @@ from fbchat import log, Client
 # Subclass fbchat.Client and override required methods
 class EchoBot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
-        self.markAsDelivered(author_id, thread_id)
-        self.markAsRead(author_id)
+        self.markAsDelivered(thread_id, message_object.uid)
+        self.markAsRead(thread_id)
 
         log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
 
