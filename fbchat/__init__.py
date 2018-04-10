416832b8b625243df067632f2ef07549079d2bfc
Mads Marquart <madsmtm@gmail.com>
2018-04-10 20:30:31 +0200

    Properly added logger, fixes #229

--- fbchat/__init__.py
+++ fbchat/__init__.py
@@ -46,3 +46,13 @@ __all__ = [
 
     'Client'
 ]
+
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
