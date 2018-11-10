63d0a1cd06731ce9442a5e57dea0aa6f057cf24e
Mads Marquart <madsmtm@gmail.com>
2018-11-10 19:54:02 +0100

    Based listening protocol on Facebook's `ChannelManager`
    - Changed parser to yield data frames instead
    - Added backoff support
    - Added `msgs_recv` hinting

--- fbchat/_listen.py
+++ fbchat/_listen.py
@@ -1,8 +1,7 @@
 import logging
+import random
 import requests
-import trio
-
-from random import randint
+from async_generator import async_generator, yield_, yield_from_
 
 from ._http import BaseSession
 
@@ -12,92 +11,127 @@ log = logging.getLogger(__name__)
 
 
 class Listener:
-    """Enables basic listening"""
+    """Handles listening for events"""
 
-    __slots__ = ("_session", "_clientid", "_sticky", "_pool", "_seq")
+    __slots__ = (
+        "_session",
+        "_clientid",
+        "_sticky_token",
+        "_sticky_pool",
+        "_seq",
+        "_msgs_recv",
+        "_backoff",
+        "mark_alive",
+    )
 
     PULL_URL = "https://0-edge-chat.facebook.com/pull"
 
-    def __init__(self, session: BaseSession) -> None:
+    def __init__(self, session: BaseSession, mark_alive=False) -> None:
         self._session = session
-        self._clientid = "{:x}".format(randint(0, 2 ** 31))
+        self.mark_alive = mark_alive
+        self._clientid = "{:x}".format(random.randint(0, 2 ** 31))
         self._clean()
 
     def _clean(self) -> None:
-        self._sticky = self._pool = None
+        self._sticky_token = self._sticky_pool = None
+        self._msgs_recv = 0
         self._seq = 0
+        self._backoff = 0
 
-    def __enter__(self) -> None:
-        self._clean()
+    def __enter__(self):
+        return self
 
     def __exit__(self, *exc) -> None:
         self._clean()
 
-    async def pull(self, mark_alive=False):
+    async def _pull(self):
         r = await self._session.get(
             self.PULL_URL,
             params={
                 "seq": self._seq,
-                "msgs_recv": 0,
-                "sticky_token": self._sticky,
-                "sticky_pool": self._pool,
+                "msgs_recv": self._msgs_recv,
+                "sticky_token": self._sticky_token,
+                "sticky_pool": self._sticky_pool,
                 "clientid": self._clientid,
-                "state": "active" if mark_alive else "offline",
-                "uid": 100002950119740,
+                "state": "active" if self.mark_alive else "offline",
             },
             timeout=60,
         )
         return r.json()
 
-    async def step(self, mark_alive=False):
-        """Do one cycle of the listening loop.
-
-        This method is useful if you want to control the listener from an
-        external event loop
-        """
-
+    @async_generator
+    async def step(self):
         try:
-            data = await self.pull(mark_alive=mark_alive)
+            data = await self._pull()
         except (requests.ConnectionError, requests.Timeout):
             # If we lost our internet connection, keep trying every minute
             await trio.sleep(60)
             return
 
-        return self.parse_raw(data)
-
-    def parse_raw(self, data):
         log.debug("Data from listening: %s", data)
 
+        await yield_from_(self._handle_protocol_data(data))
+
+    def get_backoff_delay(self):
+        if self._backoff > 0:
+            delay = min(5 * (2 ** max(0, self._backoff - 1)), 320)
+            # self.proxyDown && "proxy_down_delay_millis" in n && (x = n.proxy_down_delay_millis)
+            # self.proxyDown = False
+            return delay * random.uniform(1, 1.5)
+        return 0
+
+    def _handle_protocol_data(self, data):
+        """Handle pull protocol data, and yield data frames ready for further parsing"""
         if "seq" in data:
             self._seq = data["seq"]
+        if "s" in data:
+            self._seq = data["s"]
 
-        type_ = data["t"]
+        t = data.get("t")
 
-        if type_ == "msg":
-            self.parse_ms(data)
-        elif type_ == "heartbeat":
+        # Don't worry if you've never seen a lot of these types, this is implemented
+        # based on reading the JavaScript source for Facebook's `ChannelManager`
+        # Also, it seems like Facebook has some kind of streaming support, though I'm
+        # not quite sure what that entails yet. But a lot of of these events is for that
+
+        if t == "continue":
+            self._backoff = 0
+            log.info("Unused protocol message: %s, %s", t, data)
+
+        elif t in ("refresh", "refreshDelay"):
+            log.info("Unused protocol message: %s, %s", t, data)
+
+        elif t == "test_streaming":
+            log.info("Unused protocol message: %s, %s", t, data)
+
+        elif t == "backoff":
+            log.warning("Server told us to back off")
+            self._backoff += 1
+
+        elif t == "lb":
+            self._pool = data["lb_info"]["pool"]
+            self._sticky = data["lb_info"]["sticky"]
+
+        elif t == "fullReload":
+            self._backoff = 0
+            if "ms" in data:
+                for item in data["ms"]:
+                    self._msgs_recv += 1
+                    yield item
+
+        elif t == "msg":
+            self._backoff = 0
+            for item in data["ms"]:
+                self._msgs_recv += 1
+                yield item
+
+        elif t == "heartbeat":
             # Pull request refresh, no need to do anything
             pass
-        elif type_ == "fullReload":
-            self.parse_full_reload(data)
-        elif type_ == "lb":
-            self.parse_lb_info(data)
+
+        elif t == "batched":
+            for item in data["batches"]:
+                yield from self._handle_protocol_data(item)
+
         else:
-            log.info("Unknown data: %s", data)
-
-    def parse_lb_info(self, data):
-        self._pool = data["lb_info"]["pool"]
-        self._sticky = data["lb_info"]["sticky"]
-
-    def parse_full_reload(self, data):
-        if "ms" in data:
-            self.parse_ms(data)
-
-    def parse_ms(self, data):
-        for item in data["ms"]:
-            self.parse(item, item["type"])
-
-    def parse(self, data, type_):
-        if type_ == "qprimer":
-            return "QPrimer(Event)"
-        log.info("Unknown msg: %s", data)
+            log.error("Unknown protocol message: %s, %s", t, data)
