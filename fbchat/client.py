c1861627fbb4e2b87a4975038eb6dba1a30336f7
Mads Marquart <madsmtm@gmail.com>
2019-02-24 23:06:29 +0100

    Make deprecated public `client` module

---
+++ fbchat/client.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+"""This file is here to maintain backwards compatability."""
+from __future__ import unicode_literals
+
+from .utils import *
+from .models import *
+from .graphql import *
+from ._client import Client
