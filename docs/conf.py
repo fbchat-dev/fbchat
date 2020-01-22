41f367a61b32afe3d89c27eeacefa3430b2279db
Mads Marquart <madsmtm@gmail.com>
2020-01-22 03:29:26 +0100

    Use sphinx-autodoc-typehints

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
     "sphinxcontrib.spelling",
+    "sphinx_autodoc_typehints",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
