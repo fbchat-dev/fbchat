4491df9c30d6fca6e9130244be1323f2dfd888fa
Raphaël Côté <raphael.cote@ellipse-synergie.com>
2020-05-31 23:45:38 -0400

    Black to the rescue!

--- fbchat/_session.py
+++ fbchat/_session.py
@@ -3,13 +3,44 @@ import brotli
 
 
 class Session(requests.Session):
-    def request(self, method, url,
-            params=None, data=None, headers=None, cookies=None, files=None,
-            auth=None, timeout=None, allow_redirects=True, proxies=None,
-            hooks=None, stream=None, verify=None, cert=None, json=None):
-        resp = requests.Session.request(self, method, url, params, data, headers, cookies, files,
-                                 auth, timeout, allow_redirects, proxies, hooks, stream, verify,
-                                 cert, json)
+    def request(
+        self,
+        method,
+        url,
+        params=None,
+        data=None,
+        headers=None,
+        cookies=None,
+        files=None,
+        auth=None,
+        timeout=None,
+        allow_redirects=True,
+        proxies=None,
+        hooks=None,
+        stream=None,
+        verify=None,
+        cert=None,
+        json=None,
+    ):
+        resp = requests.Session.request(
+            self,
+            method,
+            url,
+            params,
+            data,
+            headers,
+            cookies,
+            files,
+            auth,
+            timeout,
+            allow_redirects,
+            proxies,
+            hooks,
+            stream,
+            verify,
+            cert,
+            json,
+        )
         return Response(resp)
 
 
@@ -19,7 +50,10 @@ class Response:
 
     @property
     def content(self):
-        if 'content-encoding' in self.base_response.headers and self.base_response.headers["content-encoding"] == 'br':
+        if (
+            "content-encoding" in self.base_response.headers
+            and self.base_response.headers["content-encoding"] == "br"
+        ):
             return brotli.decompress(self.base_response.content)
         return self.base_response.content
 
--- tests/test_session.py
+++ tests/test_session.py
@@ -12,12 +12,12 @@ class Expando(object):
 
 
 def test_session_request_returns_local_response_type():
-    r = Session().request('GET', "https://google.com")
+    r = Session().request("GET", "https://google.com")
     assert type(r) == Response
 
 
 def test_response_behaves_normally_through___getattr__():
-    r = Session().request('GET', "https://google.com")
+    r = Session().request("GET", "https://google.com")
 
     # not explicitely defined in Response class
     assert len(r.text) > 0
@@ -25,21 +25,17 @@ def test_response_behaves_normally_through___getattr__():
 
 def test_response_converts_brotli_compression():
     fake_base_ressource = Expando()
-    fake_base_ressource.content = brotli.compress(b'<html></html>')
-    fake_base_ressource.headers = {
-        'content-encoding': 'br'
-    }
+    fake_base_ressource.content = brotli.compress(b"<html></html>")
+    fake_base_ressource.headers = {"content-encoding": "br"}
     r = Response(fake_base_ressource)
-    assert r.content == b'<html></html>'
-    assert r.content != brotli.compress(b'<html></html>')
+    assert r.content == b"<html></html>"
+    assert r.content != brotli.compress(b"<html></html>")
 
 
 def test_response_skips_unsupported_compression():
     fake_base_ressource = Expando()
-    fake_base_ressource.content = brotli.compress(b'<html></html>')
-    fake_base_ressource.headers = {
-        'content-encoding': 'what-the-brot'
-    }
+    fake_base_ressource.content = brotli.compress(b"<html></html>")
+    fake_base_ressource.headers = {"content-encoding": "what-the-brot"}
     r = Response(fake_base_ressource)
-    assert r.content == brotli.compress(b'<html></html>')
-    assert r.content != b'<html></html>'
+    assert r.content == brotli.compress(b"<html></html>")
+    assert r.content != b"<html></html>"
